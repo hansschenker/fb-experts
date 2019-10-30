debc5673e8267d9efbfd59cf34de46969507b27b
hansschenker <hans.schenker@windowslive.com>
2019-10-30 11:40:22 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PathNotFoundComponent
   ],
   imports: [
     BrowserModule,
