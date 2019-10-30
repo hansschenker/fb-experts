4820b1d1eee35011214181d86e580ad46500414a
hansschenker <hans.schenker@windowslive.com>
2019-10-30 11:50:22 +0100

    add ngmodule experts

---
+++ src/app/experts/components/expert-form/expert-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExpertFormComponent } from './expert-form.component';
+
+describe('ExpertFormComponent', () => {
+  let component: ExpertFormComponent;
+  let fixture: ComponentFixture<ExpertFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ExpertFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExpertFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/experts/experts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hs-experts',
+  templateUrl: './experts.component.html',
+  styleUrls: ['./experts.component.scss']
+})
+export class ExpertsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
